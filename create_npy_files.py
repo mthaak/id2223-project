fc7ae0e415346efb2396170dbdafad65a6448ae8
mthaak <martin.ter.haak@gmail.com>
2018-01-08 16:41:00 +0100

    Made the whole wikiart dataset used for the final dataset

--- create_npy_files.py
+++ create_npy_files.py
@@ -6,15 +6,14 @@ from skimage import io, img_as_float
 from skimage.transform import resize
 
 wikiart_dir = 'data/wikiart'
-input_dir = 'data/wikiart_sample_csv'
-output_dir = 'data/npy_256x256'
+input_dir = 'data/wikiart_csv'
+output_dir = 'data/npy_256x256_all'
 
 resize_height = 272
 resize_width = 272
 output_height = 256
 output_width = 256
 output_channels = 3
-output_num = 1
 
 # Create output directory if not exists
 if not os.path.exists(output_dir):
@@ -29,37 +28,56 @@ def create_npy_files(input_filepath, val=False):
     data = np.loadtxt(input_file, dtype=str, delimiter=',')
     n = len(data)
 
-    # Create images and labels array
-    images = np.empty((n * output_num, output_height, output_width, 3), dtype=np.float16)
-    labels = np.empty(n * output_num, dtype=np.uint16)
-    random.seed(0)  # fixed seed
-    for i, image_file in enumerate(data[:, 0]):
-        image = img_as_float(io.imread(wikiart_dir + '/' + image_file))
-        image = resize(image, (resize_height, resize_width))
-        for j in range(output_num):
-            x1 = random.randint(0, resize_width - output_width - 1)
-            y1 = random.randint(0, resize_height - output_height - 1)
+    splits = 10
+    step = n // splits
+    intervals = [(a, min(a + step, n)) for a in range(0, n, step)]
+
+    for split, (a, b) in enumerate(intervals):
+        # Create images and labels array
+        images = np.empty((b - a, output_height, output_width, 3), dtype=np.float16)
+        labels = np.empty(b - a, dtype=np.uint16)
+        random.seed(0)  # fixed seed
+        for i in range(b - a):
+            image_filepath = data[a + i, 0]
+            label = data[a + i, 1]
+
+            # Prepare image
+            image_read = io.imread(os.path.join(wikiart_dir, image_filepath))
+            if len(image_read.shape) == 1:
+                image_read = image_read[0]
+            image = img_as_float(image_read)
+            image = resize(image, (resize_height, resize_width))
+
+            # Decide location of crop
+            if val:
+                x1, y1 = (resize_width - output_width) // 2, (resize_height - output_height) // 2
+            else:
+                x1 = random.randint(0, resize_width - output_width - 1)
+                y1 = random.randint(0, resize_height - output_height - 1)
             x2, y2 = x1 + output_width, y1 + output_height
             cropped_image = image[y1:y2, x1:x2]
+
+            # Flip random train images horizontally
             if not val and random.randint(0, 1):
-                cropped_image = np.flip(cropped_image, axis=1)  # flip random train images horizontally
-            images[i * output_num + j] = cropped_image
-            labels[i * output_num + j] = data[i, 1]
+                cropped_image = np.flip(cropped_image, axis=1)
 
-        print("({}/{})".format(i + 1, n))
+            images[i] = cropped_image
+            labels[i] = label
 
-    if output_channels == 1:
-        images = np.average(images, axis=3, weights=[0.30, 0.59, 0.11])  # convert images to grayscale
+            print("{}/{} of {} file {}/{}".format(i + 1, b - a, "val" if val else "train", split + 1, splits))
 
-    input_filename = str(os.path.basename(input_filepath).split('.')[0])
+        if output_channels == 1:
+            images = np.average(images, axis=3, weights=[0.30, 0.59, 0.11])  # convert images to grayscale
 
-    # Save images array to npy file
-    output_images_filepath = output_dir + '/' + input_filename + '_images.npy'
-    np.save(output_images_filepath, images)
+        input_filename = str(os.path.basename(input_filepath).split('.')[0])
 
-    # Save labels array to npy file
-    output_labels_filepath = output_dir + '/' + input_filename + '_labels.npy'
-    np.save(output_labels_filepath, labels)
+        # Save images array to npy file
+        output_images_filepath = '{0}/{1}_images_{2}.npy'.format(output_dir, input_filename, str(split))
+        np.save(output_images_filepath, images)
+
+        # Save labels array to npy file
+        output_labels_filepath = '{0}/{1}_labels_{2}.npy'.format(output_dir, input_filename, str(split))
+        np.save(output_labels_filepath, labels)
 
 
 # create_npy_files(input_dir + '/artist_train.csv')
--- create_tf_records.py
+++ create_tf_records.py
@@ -1,8 +1,15 @@
+import glob
+import os
+
 import numpy as np
 import tensorflow as tf
 
-input_dir = 'data/npy_256x256/'
-output_dir = 'data/tfrecords_256x256/'
+input_dir = 'data/npy_256x256_all/'
+output_dir = 'data/tfrecords_256x256_all/'
+
+# Create output directory if not exists
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 
 def _int64_feature(value):
@@ -14,6 +21,8 @@ def _bytes_feature(value):
 
 
 def create_tfrecords_file(input_images_filepath, input_labels_filepath, output_filepath):
+    print(input_images_filepath, input_labels_filepath)
+
     # Load images and labels
     images = np.load(input_images_filepath)
     labels = np.load(input_labels_filepath)
@@ -21,9 +30,14 @@ def create_tfrecords_file(input_images_filepath, input_labels_filepath, output_f
 
     # Initialize writer
     writer = tf.python_io.TFRecordWriter(output_filepath)
+
     for i in range(n):
         image = images[i]
         label = labels[i]
+
+        # Cast from 0-1 float to 0-255 uint8
+        if 'float' in image.dtype.name:
+            image = (image * 256).astype(np.uint8)
 
         # Create feature
         feature = {
@@ -40,12 +54,14 @@ def create_tfrecords_file(input_images_filepath, input_labels_filepath, output_f
         # Serialize to string and write to file
         writer.write(example.SerializeToString())
 
-        print("({}/{})".format(i + 1, n))
+        print("({}/{} of {})".format(i + 1, n, input_images_filepath))
 
     writer.close()
 
 
-# create_tfrecords_file(input_dir + 'style_train_images.npy', input_dir + 'style_train_labels.npy',
-#                       output_dir + 'style_train.tfrecords')
-create_tfrecords_file(input_dir + 'style_val_images.npy', input_dir + 'style_val_labels.npy',
-                      output_dir + 'style_val.tfrecords')
+# For each pair of images and labels create a tfrecords file
+for images_filepath in glob.glob(input_dir + 'style_train_images_*') + glob.glob(input_dir + 'style_val_images_*'):
+    labels_filepath = images_filepath.replace('images', 'labels')
+    if os.path.isfile(labels_filepath):
+        output_filename = os.path.basename(images_filepath).replace('images_', '').replace('.npy', '.tfrecords')
+        create_tfrecords_file(images_filepath, labels_filepath, os.path.join(output_dir, output_filename))
